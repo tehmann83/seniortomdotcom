492898c7ec964ea9e9641578c0056ae2c109e13e
Thomas Ehmann <ehmannth@gmail.com>
2022-05-29 14:50:28 +0200

    chore: clone old page

---
+++ pages/about/index.tsx
@@ -0,0 +1,7 @@
+type Props = {};
+
+const About = (props: Props) => {
+	return <h1>Legend</h1>;
+};
+
+export default About;
---
+++ pages/app-ads.txt/index.tsx
@@ -0,0 +1,7 @@
+type Props = {};
+
+const AppAdsTxt = (props: Props) => {
+	return <div>google.com, pub-2410338466936726, DIRECT, f08c47fec0942fa0</div>;
+};
+
+export default AppAdsTxt;
